18cd7c9ba9279b2f7301687b68ef8dbd26fb1378
traustid <traustid@gmail.com>
2016-07-09 23:30:02 +0200

    Added error handler for Image Canvas creation and loading.

--- gub.colors.js
+++ gub.colors.js
@@ -49,72 +49,77 @@ var processColors = function() {
 			var imageData = body;
 
 			console.log('new Canvas.Image');
-			var image = new Canvas.Image;
-			image.src = imageData;
 
-			var canvas = new Canvas(image.width, image.height);
-			var ctx = canvas.getContext('2d');
+			try {
 
-			console.log('ctx.drawImage');
-			ctx.drawImage(image, 0, 0, image.width, image.height);
+				var image = new Canvas.Image;
+				image.src = imageData;
 
-			var imageColors3 = _.map(colorThief.createPalette(canvas, 3), function(color) {
-					return colors.colorObject(color);
-			});
-			var imageColors5 = _.map(colorThief.createPalette(canvas, 5), function(color) {
-					return colors.colorObject(color);
-			});
-			var imageColors8 = _.map(colorThief.createPalette(canvas, 8), function(color) {
-					return colors.colorObject(color);
-			});
-			var dominantColor = colors.colorObject(colorThief.getDominantColor(canvas));
+				var canvas = new Canvas(image.width, image.height);
+				var ctx = canvas.getContext('2d');
 
-			var colorData = {
-				dominant: dominantColor,
-				colors: {
-					three: imageColors3,
-					five: imageColors5,
-					five_mapped: _.map(imageColors5, function(color) {
-						var mappedColor = colors.mapColorToPalette(color.rgb);
+				console.log('ctx.drawImage');
+				ctx.drawImage(image, 0, 0, image.width, image.height);
 
-						return colors.colorObject(mappedColor);
-					}),
-					eight_mapped: _.map(imageColors8, function(color) {
-						var mappedColor = colors.mapColorToPalette(color.rgb);
-
-						return colors.colorObject(mappedColor);
-					}),
-				}
-			};
-
-			hit._source.color = colorData;
-
-			var options = {
-				host: '127.0.0.1',
-				port: 9200,
-				path: '/arosenius/artwork/'+hit._id+'/_update',
-				method: 'POST'
-			};
-
-			var req = http.request(options, function(resp){
-				resp.on('data', function(chunk){
-					console.log('resp.on: data');
-					console.log('hitIndex: '+hitIndex+', hits.length: '+hits.length);
-			    	if (hitIndex < hits.length) {
-			    		hitIndex++;
-
-			    		processColors();
-			    	}
+				var imageColors3 = _.map(colorThief.createPalette(canvas, 3), function(color) {
+						return colors.colorObject(color);
 				});
-			}).on("error", function(e){
-				console.log("Got error: " + e.message);
-			});
+				var imageColors5 = _.map(colorThief.createPalette(canvas, 5), function(color) {
+						return colors.colorObject(color);
+				});
+				var imageColors8 = _.map(colorThief.createPalette(canvas, 8), function(color) {
+						return colors.colorObject(color);
+				});
+				var dominantColor = colors.colorObject(colorThief.getDominantColor(canvas));
 
-			req.write(JSON.stringify({
-				doc: hit._source
-			}));
-			req.write('\n');
-			req.end();
+				var colorData = {
+					dominant: dominantColor,
+					colors: {
+						three: imageColors3,
+						five: imageColors5,
+						five_mapped: _.map(imageColors5, function(color) {
+							var mappedColor = colors.mapColorToPalette(color.rgb);
+
+							return colors.colorObject(mappedColor);
+						}),
+						eight_mapped: _.map(imageColors8, function(color) {
+							var mappedColor = colors.mapColorToPalette(color.rgb);
+
+							return colors.colorObject(mappedColor);
+						}),
+					}
+				};
+
+				hit._source.color = colorData;
+
+				var options = {
+					host: '127.0.0.1',
+					port: 9200,
+					path: '/arosenius/artwork/'+hit._id+'/_update',
+					method: 'POST'
+				};
+
+				var req = http.request(options, function(resp){
+					resp.on('data', function(chunk){
+						console.log('resp.on: data');
+						console.log('hitIndex: '+hitIndex+', hits.length: '+hits.length);
+				    	if (hitIndex < hits.length) {
+				    		hitIndex++;
+
+				    		processColors();
+				    	}
+					});
+				}).on("error", function(e){
+					console.log("Got error: " + e.message);
+				});
+
+				req.write(JSON.stringify({
+					doc: hit._source
+				}));
+				req.write('\n');
+				req.end();
+			}
+			catch(e) {}
 		}
 	});
 };
