be99db2b9fa63c7b974e4ef2321c7ea7bc70ab54
traustid <traustid@gmail.com>
2017-03-25 13:35:55 +0100

    Updated imageSizes script to handle multi-image documents.

--- imageSizes.js
+++ imageSizes.js
@@ -3,9 +3,14 @@ var fs = require('fs');
 var elasticsearch = require('elasticsearch');
 
 var Canvas = require('canvas');
+var colorThief = require('thief');
+var Vibrant = require('node-vibrant');
+var chroma = require('chroma-js');
 
 var request = require("request");
 var http = require('http');
+
+var colors = require('./arosenius.color_utils');
 var config = require('./config');
 
 var data = [];
@@ -20,90 +25,130 @@ var hits = [];
 client.search({
 	index: 'arosenius',
 	type: 'artwork',
-	q: '_missing_: imagesize',
+	q: '*',
 	size: 10000
 }, function(err, response) {
 	hits = response.hits.hits;
 
-	processSizes();
+	processColors();
 
 });
 
 var hitIndex = 0;
-var processSizes = function() {
-	console.log('processSizes: hitIndex: '+hitIndex);
+var processColors = function() {
+	console.log('processColors: hitIndex: '+hitIndex);
 	var hit = hits[hitIndex];
 
-	var imagePath = config.gub_image_path+'/'+hit._source.image+'.'+config.image_type;
+	var imageIndex = 0;
 
-	console.log('readFileSync: '+imagePath);
+	var writeDocAndContinue = function() {
+		console.log(hit.images);
+		var options = {
+			host: '127.0.0.1',
+			port: 9200,
+			path: '/arosenius/artwork/'+hit._id+'/_update',
+			method: 'POST'
+		};
 
-	request.get({
-		uri: imagePath,
-		encoding: null
-	}, function(err, resp, body) {
-		if (err) {
-			console.log(err);
-			if (hitIndex < hits.length) {
-				hitIndex++;
+		var req = http.request(options, function(resp) {
+			console.log('update '+hit._id);
+			resp.on('data', function(chunk){
+				console.log('resp.on: data');
+				console.log('hitIndex: '+hitIndex+', hits.length: '+hits.length);
+			
+				if (hitIndex < hits.length-1) {
+					hitIndex++;
+					processColors();
+				}
+			});
+		}).on("error", function(e){
+			console.log("Got error: " + e.message);
+		});
 
-				processSizes();
-			}
+		req.write(JSON.stringify({
+			doc: hit._source
+		}));
+		req.write('\n');
+		req.end();
+	}
+
+	var processImage = function() {
+		console.log('imageIndex: '+imageIndex);
+		console.log('hit._source.images.length: '+hit._source.images.length);
+		console.log('imageIndex < hit._source.images.length = '+(imageIndex < hit._source.images.length));
+		console.log(hit._source.images[imageIndex]);
+		console.log(hit._source.title);
+		var image = hit._source.images[imageIndex];
+
+		if (image) {
+			var imagePath = config.gub_image_path+'/'+image.image+'.'+config.image_type;
+
+			console.log('load: '+imagePath);
+
+			request.get({
+				uri: imagePath,
+				encoding: null
+			}, function(err, resp, body) {
+				if (err) {
+					console.log(err);
+					if (imageIndex < hit._source.images.length-1) {
+						imageIndex++;
+
+						processImage();
+					}
+					else {
+						writeDocAndContinue();
+					}
+				}
+				else {
+					var imageData = body;
+
+					console.log('new Canvas.Image');
+
+					try {
+
+						var image = new Canvas.Image;
+						image.src = imageData;
+
+						hit._source.images[imageIndex].imagesize = {
+							width: image.width,
+							height: image.height
+						};
+
+						if (imageIndex < hit._source.images.length-1) {
+							imageIndex++;
+
+							processImage();
+						}
+						else {
+							writeDocAndContinue();
+						}
+					}
+					catch(e) {
+						console.log(e);
+						if (imageIndex < hit._source.images.length-1) {
+							imageIndex++;
+
+							processImage();
+						}
+						else {
+							writeDocAndContinue();
+						}
+					}
+				}
+			});
 		}
 		else {
-			var imageData = body;
+			if (imageIndex < hit._source.images.length-1) {
+				imageIndex++;
 
-			console.log('new Canvas.Image');
-
-			try {
-
-				var image = new Canvas.Image;
-				image.src = imageData;
-
-				var canvas = new Canvas(image.width, image.height);
-
-				hit._source.imagesize = {
-					width: image.width,
-					height: image.height
-				};
-
-				var options = {
-					host: '127.0.0.1',
-					port: 9200,
-					path: '/arosenius/artwork/'+hit._id+'/_update',
-					method: 'POST'
-				};
-
-				var req = http.request(options, function(resp) {
-					console.log('update '+hit._id);
-					resp.on('data', function(chunk){
-						console.log('resp.on: data');
-						console.log('hitIndex: '+hitIndex+', hits.length: '+hits.length);
-						if (hitIndex < hits.length) {
-							hitIndex++;
-
-							processSizes();
-						}
-					});
-				}).on("error", function(e){
-					console.log("Got error: " + e.message);
-				});
-
-				req.write(JSON.stringify({
-					doc: hit._source
-				}));
-				req.write('\n');
-				req.end();
-
+				processImage();
 			}
-			catch(e) {
-				console.log(e);
-				if (hitIndex < hits.length) {
-					hitIndex++;
-
-					processSizes();
-				}
+			else {
+				writeDocAndContinue();
 			}
 		}
-	});
+	}
+
+	processImage();
 };
